c74cb407c6342405430b051bfc532d6e51d0ff1d
Costin Alexandru <costinalexandru2005@gmail.com>
2026-03-30 23:10:53 +0300

    am incercat si eu sa fac trainul de la tree

--- Random_Forest/Implemet.py
+++ Random_Forest/Implemet.py
@@ -1,14 +1,49 @@
 import numpy
 class Tree:
     def __init__(self, date):
-        self.best = [numpy.random.uniform(numpy.min(date[:-1, i]), numpy.max(date[:-1, i])) for i in range(len(date[:-1, 0]))]
+        self.best = numpy.zeros(date.shape[1] - 1)
         self.date = date.copy()
 
-    def Train(self):
+
+    def calculate_gini(self, count0, count1):
+        total = count0 + count1
+        if total == 0:
+            return 0
+
+        p0 = count0/total
+        p1 = count1/total
+        return 1-(p0**2 + p1**2)
 
 
 
-        pass
+    def Trainv2(self):
+        for col in range(self.date.shape[1] - 1):
+            gini_best = 999
+            for threshold in self.date[:, col]:
+                left_mask = self.date[:, col] <= threshold
+                right_mask = self.date[:, col] > threshold
+
+                left_labels = self.date[left_mask, -1]
+                right_labels = self.date[right_mask, -1]
+
+                left_0 = numpy.sum(left_labels == 0)
+                left_1 = numpy.sum(left_labels == 1)
+                right_0 = numpy.sum(right_labels == 0)
+                right_1 = numpy.sum(right_labels == 1)
+
+                gini_left = self.calculate_gini(left_0, left_1)
+                gini_right = self.calculate_gini(right_0, right_1)
+
+                total_left = len(left_labels)
+                total_right = len(right_labels)
+                total_rows = len(self.date)
+
+                gini_split = (total_left / total_rows) * gini_left + (total_right / total_rows) * gini_right
+
+                if gini_split < gini_best:
+                    gini_best = gini_split
+                    self.best[col] = threshold
+
 
     #am un model de train in folderul meu de ml din C
     def Choice_recursiv(self, tuplu, index):
