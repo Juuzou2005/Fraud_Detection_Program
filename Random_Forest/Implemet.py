7d36cc7c4cbcdc6241f674119444d73b1e4770e4
Costin Alexandru <costinalexandru2005@gmail.com>
2026-03-30 17:43:50 +0300

    mimimimi

--- Random_Forest/Implemet.py
+++ Random_Forest/Implemet.py
@@ -1,7 +1,7 @@
 import numpy
 class Tree:
     def __init__(self, date):
-        self.optims = [numpy.random.uniform(numpy.min(date[:, i]), numpy.max(date[:, i])) for i in range(len(date[:, 0]))]
+        self.best = [numpy.random.uniform(numpy.min(date[:, i]), numpy.max(date[:, i])) for i in range(len(date[:, 0]))]
 
 
     #am un model de train in folderul meu de ml din C
@@ -9,5 +9,13 @@ class Tree:
         pass
     
 
-    def Choice(self):
+    def Choice(self, tuplu):
+        if tuplu[0] < self.best[0]: #v1
+            if tuplu[1] < self.best[1]: #v2
+                
+
+        else:
+
+            if tuplu[2] < self.best[2]: #v3
+
         pass
