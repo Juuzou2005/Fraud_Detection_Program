31c6f4abe517bceab39bfc3bb4e1987871ef1e75
Costin Alexandru <costinalexandru2005@gmail.com>
2026-03-30 21:48:21 +0300

    am terminat cu parcurgerea arborelui de decizii

--- Random_Forest/Implemet.py
+++ Random_Forest/Implemet.py
@@ -1,21 +1,27 @@
 import numpy
 class Tree:
     def __init__(self, date):
-        self.best = [numpy.random.uniform(numpy.min(date[:, i]), numpy.max(date[:, i])) for i in range(len(date[:, 0]))]
+        self.best = [numpy.random.uniform(numpy.min(date[:-1, i]), numpy.max(date[:-1, i])) for i in range(len(date[:-1, 0]))]
+        self.date = date.copy()
 
+    def Train(self):
+
+
+
+        pass
 
     #am un model de train in folderul meu de ml din C
-    def Train(self):
-        pass
-    
-
-    def Choice(self, tuplu):
-        if tuplu[0] < self.best[0]: #v1
-            if tuplu[1] < self.best[1]: #v2
-                
-
+    def Choice_recursiv(self, tuplu, index):
+        if tuplu[index] <= self.best[index]:
+            left_index = 2 * index + 1
+            if left_index >= len(self.best):
+                return 0
+            else:
+                return self.Choice_recursiv(tuplu, left_index)
         else:
+            right_index = 2 * index + 2
+            if right_index >= len(self.best):
+                return 1
+            else:
+                return self.Choice_recursiv(tuplu, right_index)
 
-            if tuplu[2] < self.best[2]: #v3
-
-        pass
